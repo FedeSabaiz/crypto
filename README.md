17215374d5d88df58a6f78ddd4c334a07b1e8c24
Federico <federico.gomez.sa@gmail.com>
2019-10-25 10:07:10 -0500

    Iniciando el proyecto de crypto- convert!

---
+++ README.md
@@ -0,0 +1,1 @@
+# Esta es un proyecto que convierte una cantidad monetaria en el valor equivalente de la cryptomoneda que elijas.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return ( 
+        <h1>PROYECTO CRYPTO-CONVERT</h1>
+     );
+}
+ 
+export default App;
